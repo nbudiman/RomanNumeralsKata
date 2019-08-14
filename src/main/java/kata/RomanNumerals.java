f8023cfe1a10978f685f118d654ee558f38940a1
nicole.budiman <nicole.budiman@thoughtworks.com>
2019-08-15 04:42:53 +0800

    Add implementation and test

--- src/main/java/kata/RomanNumerals.java
+++ src/main/java/kata/RomanNumerals.java
@@ -1,5 +1,49 @@
 package kata;
 
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RomanNumerals {
+
+    private List<Pair<String, Integer>> symbolNumberMap;
+
+    public RomanNumerals() {
+        this.symbolNumberMap = new ArrayList<>();
+        this.symbolNumberMap.add(new Pair<>("C", 100));
+        this.symbolNumberMap.add(new Pair<>("XC", 90));
+        this.symbolNumberMap.add(new Pair<>("L", 50));
+        this.symbolNumberMap.add(new Pair<>("XL", 40));
+        this.symbolNumberMap.add(new Pair<>("X", 10));
+        this.symbolNumberMap.add(new Pair<>("IX", 9));
+        this.symbolNumberMap.add(new Pair<>("V", 5));
+        this.symbolNumberMap.add(new Pair<>("IV", 4));
+        this.symbolNumberMap.add(new Pair<>("I", 1));
+    }
+
+    public String digit(String symbol, int num, int divisor) {
+        String romanNum = "";
+        int quotient = num / divisor ;
+
+        for (int i = 0; i < quotient; i++) {
+            romanNum += symbol;
+        }
+
+        return romanNum;
+    }
+
+
+    public String convert(int arabicNum) {
+        String romanNum = "";
+
+        for (int i = 0; i < symbolNumberMap.size(); i++) {
+            Pair<String, Integer> symbolValuePair = symbolNumberMap.get(i);
+            romanNum += digit(symbolValuePair.getKey(), arabicNum, symbolValuePair.getValue());
+            arabicNum = arabicNum % symbolValuePair.getValue();
+        }
+
+        return romanNum;
+    }
 
 }
--- src/test/java/kata/TestHello.java
+++ src/test/java/kata/TestHello.java
@@ -1,12 +1,76 @@
 package kata;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestHello {
+
+    private RomanNumerals romannumerals;
+
+    @Before
+    public void setUp() throws Exception {
+        romannumerals = new RomanNumerals();
+    }
+
     @Test
-    public void shouldWork() {
-        assertEquals(1, 1);
+    public void shouldReturnIWhenGiven1() {
+        assertEquals("I", romannumerals.convert(1));
+    }
+
+    @Test
+    public void shouldReturnVWhenGiven5() {
+        assertEquals("V", romannumerals.convert(5));
+    }
+
+    @Test
+    public void shouldReturnXWhenGiven10() {
+        assertEquals("X", romannumerals.convert(10));
+    }
+
+    @Test
+    public void shouldReturnIIWhenGiven2() {
+        assertEquals("II", romannumerals.convert(2));
+    }
+
+    @Test
+    public void shouldReturnIVWhenGiven4() {
+        assertEquals("IV", romannumerals.convert(4));
+    }
+
+    @Test
+    public void shouldReturnVIWhenGiven6() {
+        assertEquals("VI", romannumerals.convert(6));
+    }
+
+    @Test
+    public void shouldReturnXIWhenGiven11() {
+        assertEquals("XI", romannumerals.convert(11));
+    }
+
+    @Test
+    public void shouldReturnXXIWhenGiven21() {
+        assertEquals("XXI", romannumerals.convert(21));
+    }
+
+    @Test
+    public void shouldReturnXXVIIIWhenGiven28() {
+        assertEquals("XXVIII", romannumerals.convert(28));
+    }
+
+    @Test
+    public void shouldREturnMXXIIIWhenGIven123() {
+        assertEquals("CXXIII", romannumerals.convert(123));
+    }
+
+    @Test
+    public void shouldReturnXCIWhenGIven91() {
+        assertEquals("XCI", romannumerals.convert(91));
+    }
+
+    @Test
+    public void shouldReturnXCIXWhenGiven99() {
+        assertEquals("XCIX", romannumerals.convert(99));
     }
 }
